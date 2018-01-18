fa7a455ae6f9156735ee0813e599354a0e98c07b
yarshure <yarshure@gmail.com>
2018-01-18 21:48:58 +0800

    add kcp test

--- macTest/KcpTestVC.swift
+++ macTest/KcpTestVC.swift
@@ -8,21 +8,41 @@
 
 import Cocoa
 import Xcon
+import os.log
 class KcpTestVC: NSViewController ,XconDelegate{
     func didDisconnect(_ socket: Xcon, error: Error?) {
-          print("didDisconnect ")
+        let ts = Date().timeIntervalSince(start)
+        if #available(OSX 10.12, *) {
+            os_log("Xcon: %.2f", log: .default, type: .debug, ts)
+        } else {
+            // Fallback on earlier versions
+        }
+        
     }
     
     func didReadData(_ data: Data, withTag: Int, from: Xcon) {
         print("didReadData \(data as NSData)")
+        let ts = Date().timeIntervalSince(start)
+        if #available(OSX 10.12, *) {
+            os_log("Xcon: didReadData:%.2f", log: .default, type: .debug, ts)
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     func didWriteData(_ data: Data?, withTag: Int, from: Xcon) {
-         print("didReadData \(data as? NSData)")
-         con.forceDisconnect()
+       
+         //print("didReadData \(data as? NSData)")
+         //con.forceDisconnect()
     }
     
     func didConnect(_ socket: Xcon) {
+        let ts = Date().timeIntervalSince(start)
+        if #available(OSX 10.12, *) {
+            os_log("Xcon: didConnect: %.2f", log: .default, type: .debug, ts)
+        } else {
+            // Fallback on earlier versions
+        }
         let data = "GET / HTTP/1.1\r\nHost: twitter.com\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\n\r\n".data(using: .utf8)
         con.writeData(data!, withTag: 0)
         
@@ -30,6 +50,7 @@ class KcpTestVC: NSViewController ,XconDelegate{
     
 
     var con:Xcon!
+    var start:Date = Date()
     let queue = DispatchQueue.init(label: "callback.kcp")
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +58,7 @@ class KcpTestVC: NSViewController ,XconDelegate{
         guard let p = SFProxy.createProxyWithLine(line: kcptun, pname: "CN2") else{
             fatalError()
         }
+        start = Date()
         p.kcptun = true
         con = Xcon.socketFromProxy(p, targetHost: "twitter.com", Port: 80, delegate: self, queue: queue,sessionID:0)
         // Do view setup here.
